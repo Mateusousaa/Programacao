cb3089749eb6a318b03cd89993e1919ca0e9d26c
Mateusousaa <93954252+Mateusousaa@users.noreply.github.com>
2021-11-23 00:59:33 -0300

    Add files via upload

--- EX.26.c
+++ EX.26.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int R, pos, num1, num2;
-  printf("Digite um numero positivo: ");
+int main(int argc, char *argv[]){
+int num=0, R, pos;
+  printf("Digite um valor positivo: ");
   scanf("%i", &pos);
 
-  while(pos<0){
-      printf("Digite um numero positivo: ");
-      scanf("%i", &pos);
-  }
-  printf("\nDigite dois numeros, onde o segundo seja maior que o primeiro\n");
-  printf("Primeiro numero: ");
-  scanf("%i", &num1);
-  printf("\nSegundo numero: ");
-  scanf("%i", &num2);
+  while(pos < 0){
+      printf("\n\nDigite um valor positivo: ");
+      scanf("%i", &pos);}
 
-  while(num2 < num1){
-    printf("\nSegundo numero: ");
-    scanf("%i", &num2);}
+  while(num != 11){
+    R = pos * num;
+    printf("\n%i x %i = %i\n", pos, num, R);
+    num = num + 1;}
 
-  num2 = num2 - 1; 
-
-  while(num1 != num2){ 
-  R = pos * num2;
-  printf("%i * %i = %i\n", pos, num2, R  );
-  num2 = num2 - 1;}
-
-  return 0;
-}
+return 0;}
